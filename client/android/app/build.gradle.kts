60959c5f4b76b5c3d7b7da2e69dd2b173b3386b9
anshRS <anshraj0426@gmail.com>
2025-05-12 15:34:38 +0530

    chore(ui): resolve android NDK version mismatch

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.xautoflow"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Modified this: flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
