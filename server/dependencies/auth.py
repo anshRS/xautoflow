2aba939044820dcaff4f2b043fb54a25f1bc6093
anshRS <anshraj0426@gmail.com>
2025-05-14 12:38:13 +0530

    feat(server): implement supabase client authenticated as the current user

--- server/dependencies/auth.py
+++ server/dependencies/auth.py
@@ -1,12 +1,13 @@
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi import Depends
+from supabase import Client, ClientOptions, create_client
 from server.utils.exceptions import UnauthorizedException
-from server.config.db import supabase
 from server.settings import settings
 import jwt
 
 security = HTTPBearer()
 
+# Decode and verify JWT manually
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     try:
         token = credentials.credentials 
@@ -16,8 +17,20 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
         user_id = payload.get('sub')
         if user_id is None:
             raise UnauthorizedException("Invalid user credentials")
+        payload['token'] = token
         return payload        
     except jwt.ExpiredSignatureError:
         raise UnauthorizedException("Token has expired")
     except jwt.PyJWKError:
         raise UnauthorizedException("Invalid auth credentials")
+
+# Supabase client authenticated as the current user
+def get_user_supabase_client(user=Depends(get_current_user)) -> Client:
+    options = ClientOptions(
+        headers={
+            "Authorization": f"Bearer {user['token']}"
+        }
+    )
+
+    client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY, options=options)
+    return client
